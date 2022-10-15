637f69821ca13124c259a5befb71b0ebca83baee
affect <nabil.boufeldja@gmail.com>
2022-10-15 16:29:34 +0200

    Fill orders

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -6,7 +6,44 @@ import "./Token.sol";
 contract Exchange {
     address public feeAccount;
     uint256 public feePercent;
+    uint256 public ordersCount = 0;
     mapping(address => mapping(address => uint256)) public deposit;
+    mapping(uint256 => _Order) public orders;
+    mapping(uint256 => bool) public orderCancelled;
+    mapping(uint256 => bool) public orderFilled;
+
+    struct _Order {
+        address orderingUser;
+        uint256 timestamp;
+        uint256 id;
+        address _tokenGive; uint256 _amountGive;
+        address _tokenGet; uint256 _amountGet;
+    }
+
+    event Trade(
+        address orderingUser,
+        address fulfillingUser,
+        uint256 timestamp,
+        uint256 id,
+        address _tokenGive, uint256 _amountGive,
+        address _tokenGet, uint256 _amountGet
+    );
+
+    event Order(
+        address orderingUser,
+        uint256 timestamp,
+        uint256 id,
+        address _tokenGive, uint256 _amountGive,
+        address _tokenGet, uint256 _amountGet
+    );
+
+    event Cancel(
+        address orderingUser,
+        uint256 timestamp,
+        uint256 id,
+        address _tokenGive, uint256 _amountGive,
+        address _tokenGet, uint256 _amountGet
+    );
 
     event Deposit(
         address _token,
@@ -40,5 +77,107 @@ contract Exchange {
         Token(_token).transferFrom(msg.sender, address(this), _amount);
         deposit[_token][msg.sender] += _amount;
         emit Deposit(_token, msg.sender, _amount, deposit[_token][msg.sender]);
+    }
+
+    function makeOrder(
+        address _tokenGive, uint256 _amountGive,
+        address _tokenGet, uint256 _amountGet) 
+        public
+    {
+        require(_tokenGet != address(0),
+            "f/ makeOrder() : Address(0) not allowed");
+        require(_tokenGive != address(0),
+            "f/ makeOrder() : Address(0) not allowed");
+        require(deposit[_tokenGive][msg.sender] >= _amountGive,
+            "f/ makeOrder() : Amount must be equal or less than deposit");
+
+        orders[ordersCount] =   _Order(
+                                    msg.sender,
+                                    block.timestamp,
+                                    ordersCount,
+                                    _tokenGive, _amountGive,
+                                    _tokenGet, _amountGet
+                                );
+
+        emit Order(
+            msg.sender,
+            block.timestamp,
+            ordersCount,
+            _tokenGive, _amountGive,
+            _tokenGet, _amountGet
+        );
+        ordersCount += 1;
+    }
+
+    function cancelOrder(uint256 _id) public {
+        _Order storage _order = orders[_id];
+        require(msg.sender == _order.orderingUser,
+            "f/ cancelOrder() : Only the ordering user can cancel the order");
+        require(_order.timestamp != 0,
+            "f/ cancelOrder() : Non-existant order can't be cancelled");
+        orderCancelled[_id] = true;
+
+        emit Cancel(
+            msg.sender,
+            block.timestamp,
+            _order.id,
+            _order._tokenGive, _order._amountGive,
+            _order._tokenGet, _order._amountGet
+        );
+    }
+
+    function fillOrder(uint256 _id) public {
+        _Order storage order = orders[_id];
+
+        uint256 _feeAmount = (order._amountGet * feePercent) / 100;
+        require(orders[_id].timestamp != 0,
+            "f/ fillOrder() : Non-existant order can't be fulfilled");
+        require(orderCancelled[_id] == false,
+            "f/ fillOrder() : Cancelled order cannot be fulfilled");
+        require(orderFilled[_id] == false, 
+            "f/ fillOrder() : Order has already been fulfilled");
+        require(order.orderingUser != msg.sender,
+            "f/ fillOrder() : Ordering user and fulfilling user can't be the same");
+        require(deposit[order._tokenGet][msg.sender] >= (order._amountGet + _feeAmount),
+            "f/ fillOrder() : No enought fund to fulfill order and pay fees");
+
+        _trade(
+            order.orderingUser,
+            order._tokenGive, order._amountGive,
+            order._tokenGet, order._amountGet
+        );
+
+        emit Trade(
+            order.orderingUser,
+            msg.sender,
+            block.timestamp,
+            order.id,
+            order._tokenGive, order._amountGive,
+            order._tokenGet, order._amountGet
+        );
+
+        orderFilled[_id] = true;
+    }
+
+    function _trade(
+        address _user,
+        address _tokenGive,
+        uint _amountGive,
+        address _tokenGet,
+        uint256 _amountGet)
+        internal 
+    {
+        uint256 _feeAmount = (_amountGet * feePercent) / 100;
+
+        deposit[_tokenGet][msg.sender] =
+            deposit[_tokenGet][msg.sender] - (_amountGet + _feeAmount);
+        deposit[_tokenGet][_user] = deposit[_tokenGet][_user] + _amountGet;
+
+        deposit[_tokenGet][feeAccount] =
+            deposit[_tokenGet][feeAccount] + _feeAmount;
+
+        deposit[_tokenGive][_user] = deposit[_tokenGive][_user] - _amountGive;
+        deposit[_tokenGive][msg.sender] =
+            deposit[_tokenGive][msg.sender] + _amountGive;
     }
 }
--- test/Exchange.js
+++ test/Exchange.js
@@ -6,10 +6,9 @@ describe("Exchange", () => {
     const addressZero = "0x0000000000000000000000000000000000000000";
     const feePercent = 5;
     const tokens = (n) =>
-        ethers.utils.parseUnits(n.toString(), 'ether');
+        BigInt(ethers.utils.parseUnits(n.toString(), 'ether'));
     
     const amount = tokens(1000);
-    const check = tokens(1000);
     const supply = 1000000;
     const totalSupply = tokens(supply);
 
@@ -19,11 +18,14 @@ describe("Exchange", () => {
         deployer = accounts[0];
         feeAccount = accounts[1];
         user1 = accounts[2];
+        user2 = accounts[3];
 
         const Token = await ethers.getContractFactory("Token");
         token1 = await Token.deploy('StonksCoin', 'STK', '1000000');
+        token2 = await Token.deploy('CoinCoin', 'CC', '1000000');
 
         token1.connect(deployer).transfer(user1.address, amount);
+        token2.connect(deployer).transfer(user2.address, amount);
 
         const Exchange = await ethers.getContractFactory("Exchange");
         exchange = await Exchange.deploy(
@@ -59,11 +61,11 @@ describe("Exchange", () => {
                 expect(await exchange.deposit(token1.address, user1.address)).to.equal(amount);
             });
             it("emit Deposit event", async () => {
-                expect(await event.event).to.equal("Deposit");
-                expect(await event.args._token).to.equal(token1.address);
-                expect(await event.args._user).to.equal(user1.address);
-                expect(await event.args._balance).to.equal(amount);
-                expect(await event.args._amount).to.equal(amount);
+                expect(event.event).to.equal("Deposit");
+                expect(event.args._token).to.equal(token1.address);
+                expect(event.args._user).to.equal(user1.address);
+                expect(event.args._balance).to.equal(amount);
+                expect(event.args._amount).to.equal(amount);
             });
         });
         describe("Failure", () => {
@@ -96,18 +98,204 @@ describe("Exchange", () => {
                 expect(await exchange.deposit(token1.address, user1.address)).to.equal(tokens(0));
             });
             it("emit Deposit event", async () => {
-                expect(await event.event).to.equal("Withdraw");
-                expect(await event.args._token).to.equal(token1.address);
-                expect(await event.args._user).to.equal(user1.address);
-                expect(await event.args._balance).to.equal(tokens(0));
-                expect(await event.args._amount).to.equal(amount);
+                expect(event.event).to.equal("Withdraw");
+                expect(event.args._token).to.equal(token1.address);
+                expect(event.args._user).to.equal(user1.address);
+                expect(event.args._balance).to.equal(tokens(0));
+                expect(event.args._amount).to.equal(amount);
             });
         });
         describe("Failure", async () => {
             it("reject withdraw when amount is greater than deposit", async () => {
                 await expect(exchange.connect(user1)
                     .withdrawToken(token1.address, amount)).to.be.reverted;
-            })
-        })
-    })
-})
+            });
+        });
+    });
+
+    describe("Making orders", () => {
+        let event;
+        let amountGive = tokens(100);
+        let amountGet = tokens(50);
+
+        describe("Success", async () => {
+            beforeEach(async () => {
+                const approval = await token1.connect(user1)
+                    .approve(exchange.address, amount);
+                await approval.wait();
+
+                const transaction = await exchange.connect(user1)
+                    .depositToken(token1.address, amount);
+                await transaction.wait();
+
+                const order = await exchange.connect(user1)
+                    .makeOrder(token1.address, amountGive,
+                               token2.address, amountGet);
+                let result = await order.wait();
+                event = result.events[0];
+            });
+
+            it("track token order", async () => {
+                const order = await exchange.orders(0);
+
+                expect(order._tokenGive).to.equal(token1.address);
+                expect(order._amountGive).to.equal(amountGive);
+                expect(order._tokenGet).to.equal(token2.address);
+                expect(order._amountGet).to.equal(amountGet);
+            });
+            it("emit Order event", async () => {
+                expect(event.event).to.equal("Order");
+                expect(event.args._tokenGive).to.equal(token1.address);
+                expect(event.args._amountGive).to.equal(amountGive);
+                expect(event.args._tokenGet).to.equal(token2.address);
+                expect(event.args._amountGet).to.equal(amountGet);
+                expect(event.args.timestamp).to.at.least(1);
+                expect(event.args.id).to.equal(0);
+            });
+        });
+        describe("Failure", async () => {
+            it("reject order when amount is greater than deposit", async () => {
+                    await expect(exchange.connect(user1).makeOrder(
+                        token1.address, tokens(1001),
+                        token2.address, amountGet)).to.be.reverted;
+            });
+        });
+    });
+
+    describe("Order actions", () => {
+        let event;
+        let orderID;
+        let amountGive = tokens(100);
+        let amountGet = tokens(50);
+
+        beforeEach(async () => {
+            const approval1 = await token1.connect(user1)
+                .approve(exchange.address, amount);
+            await approval1.wait();
+
+            const approval2 = await token2.connect(user2)
+                .approve(exchange.address, amount);
+            await approval2.wait();
+
+            const transaction1 = await exchange.connect(user1)
+                .depositToken(token1.address, amount);
+            await transaction1.wait();
+    
+            const transaction2 = await exchange.connect(user2)
+                .depositToken(token2.address, amount);
+            await transaction2.wait();
+
+            const order = await exchange.connect(user1)
+                .makeOrder(token1.address, amountGive,
+                        token2.address, amountGet);
+            
+            orderID = (await order.wait()).events[0].args.id;
+        });
+        describe("Cancelling orders", () => {
+            describe("Success", () => {
+                beforeEach(async () => {
+                    const cancel = await exchange.connect(user1)
+                        .cancelOrder(0);
+                    let result = await cancel.wait();
+                    event = result.events[0];
+                });
+
+                it("cancel desired order", async () => {
+                    expect(await exchange.orderCancelled(0)).to.equal(true);
+                });
+                it("emit Cancel event", async () => {
+                    expect(event.event).to.equal('Cancel');
+                    expect(event.args._tokenGive).to.equal(token1.address);
+                    expect(event.args._amountGive).to.equal(amountGive);
+                    expect(event.args._tokenGet).to.equal(token2.address);
+                    expect(event.args._amountGet).to.equal(amountGet);
+                    expect(event.args.timestamp).to.at.least(1);
+                    expect(event.args.id).to.equal(0);
+                });
+            });
+            describe("Failure", () => {
+                it("reject unauthorized user to cancel order", async () => {
+                    const order = await exchange.connect(user1)
+                        .makeOrder(token1.address, amountGive,
+                            token2.address, amountGet);
+
+                    await expect(exchange.connect(user2).cancelOrder(0)).to.be.reverted;
+                });
+                it("reject cancelling non-existant order", async () => {
+                    const order = await exchange.connect(user1)
+                        .makeOrder(token1.address, amountGive,
+                            token2.address, amountGet);
+
+                    await expect(exchange.connect(user2).cancelOrder(10)).to.be.reverted;
+                });
+            });
+
+            describe("Filling orders", () => {
+                let feeValue;
+                let user2loss;
+                describe("Success", () => {
+                    beforeEach(async () => {
+                        const fill = await exchange.connect(user2).fillOrder(orderID);
+                        const result = await fill.wait()
+                        feeValue = (amountGet * BigInt(5)) / BigInt(100);
+                        user2loss = feeValue + amountGet;
+    
+                        event = result.events[0];
+                    });
+
+                    it("trade tokens successfully", async () => {
+                        expect(await exchange.deposit(token1.address, user1.address))
+                            .to.equal(amount - amountGive);
+                        expect(await exchange.deposit(token2.address, user2.address))
+                            .to.equal(amount - user2loss);
+                        expect(await exchange.deposit(token2.address, feeAccount.address))
+                            .to.equal(feeValue);
+                        expect(await exchange.deposit(token2.address, user1.address))
+                            .to.equal(amountGet);
+                        expect(await exchange.deposit(token1.address, user2.address))
+                            .to.equal(amountGive);
+                    });
+
+                    it("emit Trade event", async () => {
+                        expect(event.event).to.equal('Trade');
+                        expect(event.args.orderingUser).to.equal(user1.address);
+                        expect(event.args.fulfillingUser).to.equal(user2.address);
+                        expect(event.args._tokenGive).to.equal(token1.address);
+                        expect(event.args._amountGive).to.equal(amountGive);
+                        expect(event.args._tokenGet).to.equal(token2.address);
+                        expect(event.args._amountGet).to.equal(amountGet);
+                        expect(event.args.timestamp).to.at.least(1);
+                        expect(event.args.id).to.equal(0);
+                    });
+                });
+                describe("Failure", () => {
+                    it("reject when order does not exist", async () => {
+                        await expect(exchange.connect(user1).fillOrder(1))
+                            .to.be.reverted;
+                    });
+                    it("reject when order has been cancelled", async () => {
+                        await exchange.connect(user1).cancelOrder(0);
+                        await expect(exchange.connect(user2).fillOrder(0))
+                            .to.be.reverted;
+                    });
+                    it("reject when order has already been fulfilled", async () => {
+                        await exchange.connect(user2).fillOrder(0);
+                        await expect(exchange.connect(user2).fillOrder(0))
+                            .to.be.reverted;
+                    });
+                    it("reject when ordering and fulfilling users are the same", async () => {
+                        await expect(exchange.connect(user1).fillOrder(0))
+                            .to.be.reverted;
+                    });
+                    it("reject if fulfilling user has no enought fund to trade and pay fee", async () => {
+                        const order = await exchange.connect(user1)
+                                        .makeOrder(token1.address, amountGive,
+                                            token2.address, amount);
+                        
+                        await expect(exchange.connect(user2).fillOrder(1)).to.be.reverted;
+                    })
+                });
+            });
+        });
+    });
+});
